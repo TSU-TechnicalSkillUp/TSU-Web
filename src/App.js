fa52623f00d17ed515e7582d04a167b323e2ccd8
Gavin.Kim <hopae.gavin@gmail.com>
2021-02-21 00:21:57 +0900

    refactor appjs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
-import Routes from "src/utils/Routes";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Main from "src/views/main";
+import Project from "./views/Project";
+import SignUp from "./views/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <Routes />
-    </div>
+    <Router>
+      <Route path="/" component={Main} />
+      <Route path="/project" component={Project} />
+      <Route path="/signup" component={SignUp} />
+    </Router>
   );
 }
 
