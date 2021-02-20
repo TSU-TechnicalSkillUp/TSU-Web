aae51a53d42944f4edb28b517ef4bfcae0c958b1
minha <etchcure@gmail.com>
2021-02-20 16:52:22 +0900

    메인 화면

---
+++ src/views/main/index.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import Button from '@material-ui/core/Button';
+
+import LoginModal from 'src/components/logInModal/index'
+
+// test 용
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function MenuAppBar() {
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [isShowLoginModal, setIsShowLoginModal] = React.useState(false);
+  const open = Boolean(anchorEl);
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleShowLogIn = () => {
+    setIsShowLoginModal(true)
+  }
+
+  let button; 
+  if (auth) {
+    button = <div>
+        <IconButton
+          aria-label="account of current user"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          onClick={handleMenu}
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorEl}
+          anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+          }}
+          open={open}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>My Page</MenuItem>
+          <MenuItem onClick={handleClose}>정보 수정</MenuItem>
+          <MenuItem onClick={handleClose}>Log Out</MenuItem>
+        </Menu>
+    </div>
+  } else {
+    button = <div>
+      <Button color="inherit" onClick={handleShowLogIn}>Login</Button>
+    </div>
+  }
+  return (
+    <div className={classes.root}>
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
+          label={auth ? 'Logout' : 'Login'}
+        />
+      </FormGroup>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Tech Skill Up
+          </Typography>
+          {button}
+        </Toolbar>
+      </AppBar>
+      <LoginModal open={isShowLoginModal} onCloseLoninModal={setIsShowLoginModal}>
+      </LoginModal>
+    </div>
+  );
+}
